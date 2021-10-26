26b4b2c1c9ff52ffa08fff459bf1d9e5561eecf0
manasyenpure <84696410+manasyenpure@users.noreply.github.com>
2021-10-26 19:58:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,50 +1,54 @@
-var canvas;
-var backgroundImage, bgImg, car1_img, car2_img, track;
-var database, gameState;
-var form, player, playerCount;
-var allPlayers, car1, car2,fuels, powerCoins;
-var cars = [];
-var fuelImage, powerCoinImage,obstacle1,obstacle1Image,obstacle2Img,obstacle2;
-//BP
-function preload() {
-  backgroundImage = loadImage("./assets/background.png");
-  car1_img = loadImage("./assets/car1.png");
-  car2_img = loadImage("./assets/car2.png");
-  track = loadImage("./assets/track.jpg");
-  fuelImage = loadImage("./assets/fuel.png");
-  powerCoinImage = loadImage("./assets/goldCoin.png");
-  lifeImage = loadImage("./assets/life.png");
-  obstacle1Image = loadImage("./assets/obstacle1.png"); 
-  obstacle2Image = loadImage("./assets/obstacle2.png");
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var score =0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var player1score =0;
+var player2score =0;
 
+var obstacleGroup,obstacle,obstacleImage;
+
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
+
+  obstacleImage = loadImage("images/mushroom.png")
 }
-
-//BP
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  createCanvas(1000, 600);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
- 
+  
 }
 
-//BP
 function draw() {
-  background(backgroundImage);
-  if (playerCount === 2) {
-    game.update(1);
-  }
-
-  if (gameState === 1) {
-    game.play();
-  }
-  if (gameState === 2) {
-    game.showLeaderboard();
-    //game.end();
-  }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
 }
